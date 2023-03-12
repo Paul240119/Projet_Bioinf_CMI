92e3e9f852eecd3976a6dcf6696cc1fd0881c121
KMoore <127398248+KMoore9916@users.noreply.github.com>
2023-03-12 23:35:17 +0100

    Update Step1-Bloc2-alignement.py

--- Step1-Bloc2-alignement.py
+++ Step1-Bloc2-alignement.py
@@ -1,1 +1,3 @@
 #Code pour programmer le Bloc2 // Partie 2 de l'Etape 1
+#Svp regardez sur le pad avant de vous y mettre, pour qu'on voit si on est d'accord sur le plan d'attaque
+#et pour qu'on le complete car je suis pas sure du tout !! ;)
