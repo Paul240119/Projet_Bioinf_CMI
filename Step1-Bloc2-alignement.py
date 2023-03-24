4f783eab4dcb0b703dc4fba1b4727bdd7f2a4b9b
KMoore <127398248+KMoore9916@users.noreply.github.com>
2023-03-24 17:57:55 +0100

    Delete Step1-Bloc2-alignement.py

--- Step1-Bloc2-alignement.py
+++
@@ -1,3 +0,0 @@
-#Code pour programmer le Bloc2 // Partie 2 de l'Etape 1
-#Svp regardez ma reflexion sur le pad avant de vous y mettre, pour qu'on voit si on est d'accord sur le plan d'attaque
-#et pour qu'on le complete car je suis pas sure du tout !! ;)
